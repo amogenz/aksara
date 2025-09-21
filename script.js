bed470923373d8b95ee08f9bd25a132265ac27f6
Ammo <thefriendscorporations@gmail.com>
2025-09-21 08:18:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-// Firebase Config (GANTI DENGAN PUNYA LU dari Firebase Console)
-const firebaseConfig = {
-    apiKey: "AIzaSyAgxJEUX-9ZvPMGplgDfNuHjwqN2N-Y3Hs",
-    authDomain: "aksara-9505c.firebaseapp.com",
-    projectId: "aksara-9505c",
-    storageBucket: "aksara-9505c.firebasestorage.app",
-    messagingSenderId: "756360596769",
-    appId: "1:756360596769:web:22db2c8b39544f95238260",
-    measurementId: "G-HWEVYGKQ0Z"
-  };
-
-// Inisialisasi Firebase
+// Inisialisasi Firebase dari config.js
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 const auth = firebase.auth();
